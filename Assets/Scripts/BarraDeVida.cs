f3347aa0e358390bc70387fb904f35a362580446
FranciscoCai <jiehaocai54@gmail.com>
2024-01-26 21:33:04 +0100

    Sprite

--- Assets/Scripts/BarraDeVida.cs
+++ Assets/Scripts/BarraDeVida.cs
@@ -8,11 +8,11 @@ public class BarraDeVida : MonoBehaviour
     [SerializeField] private Image[] Grupos;
     [SerializeField] private float[] ExperienciaNecesariaLosGrupos;
 
-    void FixedUpdate()
+    void Update()
     {
         for (int i = 0; i < Grupos.Length; i++)
         {
-            Grupos[i].fillAmount += ExperienciaNecesariaLosGrupos[i] + Time.deltaTime;
+            Grupos[i].fillAmount += (Time.deltaTime)/ ExperienciaNecesariaLosGrupos[i];
         }
     }
 }

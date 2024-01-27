78d7088cbeaa91a6ee0b7b425799c813af2ce621
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-01-27 13:20:22 +0100

    Pineda

---
+++ Assets/Scripts/Interactuable/Puerta.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour, Interactuable
+{
+    private BoxCollider2D miCollider;
+
+    void Start()
+    {
+      
+        miCollider = GetComponent<BoxCollider2D>();
+
+      
+        miCollider.isTrigger = false;
+    }
+
+    void Update()
+    {
+        
+        
+        
+            
+        
+    }
+
+   
+    public void Interactuar()
+    {
+        InvokeRepeating("Terrorism", 0f, 2f);
+
+        CambiarATrigger();
+
+
+    }
+    void Terrorism()
+    {
+        BarraDeVida.Instance.ActoTerrorista(3, 10);
+    }
+    void CambiarATrigger()
+    {
+       
+        miCollider.isTrigger = !miCollider.isTrigger;
+
+        
+    }
+}

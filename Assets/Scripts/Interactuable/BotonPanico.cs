569e5320c2ab2a8948b84b7662d22f51be65d39c
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-01-27 11:35:11 +0100

    Boton del pánico

---
+++ Assets/Scripts/Interactuable/BotonPanico.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonPanico : MonoBehaviour, Interactuable
+{
+    public bool suicido = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Interactuar()
+    {
+        suicido = true;
+           
+            
+
+
+    }
+}
---
+++ Assets/Scripts/Interactuable/Suicida.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Suicida : MonoBehaviour
+{
+    public BotonPanico botonPanico;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BotonPanico botonPanico = Object.FindFirstObjectByType<BotonPanico>();
+        bool death = botonPanico.suicido;
+
+        if (death == true)
+        {
+            Debug.Log("odio mi via");
+            animator.Play("Suicidio");
+            StartCoroutine(Muerte(1f));
+        }
+    }
+    IEnumerator Muerte(float tiempoDeEspera)
+    {
+        
+        yield return new WaitForSeconds(tiempoDeEspera);
+
+
+        Destroy(gameObject);
+    }
+}

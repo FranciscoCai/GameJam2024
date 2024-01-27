96ae83d79580852400e727e54956eeabd4e2e6ff
JesusGalvez2 <jesusgalvezalvarez04@gmail.com>
2024-01-27 11:00:38 +0100

    Escaleras

---
+++ Assets/Scripts/Interactuable/Escalera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Escalera : MonoBehaviour, Interactuable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Interactuar()
+    {
+        if (gameObject.transform.localPosition.x == 14.3f && gameObject.transform.localPosition.y == -25.24f)
+        {
+            Debug.Log(1111);
+        }
+        
+       
+    }
+}

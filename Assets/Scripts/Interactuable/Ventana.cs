1831c38a8acf863d808b3054dddef7095935b771
FranciscoCai <jiehaocai54@gmail.com>
2024-01-27 12:14:06 +0100

    Ventana

--- Assets/Scripts/Interactuable/Ventana.cs
+++ Assets/Scripts/Interactuable/Ventana.cs
@@ -1,21 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+public enum VentanaState
+{
+    Abierto, Cerrado
+}
 public class Ventana : MonoBehaviour, Interactuable
 {
+    public VentanaState estadoVentana;
+    private SpriteRenderer spriteVentana;
+    [SerializeField] private Sprite[] SpriteEstadoVentana;
+    private void Awake()
+    {
+        spriteVentana = gameObject.GetComponent<SpriteRenderer>();
+    }
     public void Interactuar()
     {
-        Debug.Log(1111);
-    }
-    void Start()
-    {
-        
+        switch (estadoVentana)
+        {
+            case VentanaState.Abierto:
+                estadoVentana = VentanaState.Cerrado;
+                break;
+            case VentanaState.Cerrado:
+                estadoVentana = VentanaState.Abierto;
+                break;
+        }
+        CambioSpriteVentana();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CambioSpriteVentana()
     {
-        
+        switch (estadoVentana)
+        {
+            case VentanaState.Abierto:
+                spriteVentana.sprite = SpriteEstadoVentana[0];
+                break;
+            case VentanaState.Cerrado:
+                spriteVentana.sprite = SpriteEstadoVentana[1];
+                break;
+        }
+    }
+    private void Start()
+    {
+        CambioSpriteVentana();
     }
 }
